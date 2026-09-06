f068acf069a6d545962fbdadb2169a6ad00370ee
Froshi <125601800+Froshi-iuri@users.noreply.github.com>
2026-09-06 01:38:18 -0500

    fix: fix Rename Usuario to Usuarios in admin registration

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Usuario, Rol
+from .models import Usuarios, Rol
 
 
 @admin.register(Rol)
@@ -8,7 +8,7 @@ class RolAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre_rol')
 
 
-@admin.register(Usuario)
+@admin.register(Usuarios)
 class UsuarioAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ('Información de Rol', {'fields': ('rol', 'activo')}),
