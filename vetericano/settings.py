aa803b9f0cdf36d27f60ba0ec6508b601c3498fc
Froshi <kg5892868@gmail.com>
2026-09-06 01:02:00 -0500

    feat: new conection to railway added

--- vetericano/settings.py
+++ vetericano/settings.py
@@ -24,11 +24,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-k8kuso8-gb$syavcny^-$
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = [
-    '.onrender.com',
-    'localhost',
-    '127.0.0.1'
-]
+# ALLOWED_HOSTS = [
+#     '.onrender.com',
+#     'localhost',
+#     '127.0.0.1'
+# ]
+ALLOWED_HOSTS = ['*']
 
 
 
