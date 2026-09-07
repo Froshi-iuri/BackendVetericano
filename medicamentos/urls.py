2b646d2e19f35ead59182089dc9a653c0cd01c56
AnaVetericano <anyta822@gmail.com>
2026-09-06 19:03:40 -0500

    Update: Creación:endpoints medicamentos

---
+++ medicamentos/urls.py
@@ -0,0 +1,11 @@
+# AnaC
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MedicamentoViewSet
+
+router = DefaultRouter()
+router.register(r'medicamentos', MedicamentoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
