573402989f0921eb5e4c0a9fafd505bfc6f3b479
JDquintero1002 <jdquintero@unicauca.edu.co>
2026-09-07 20:28:15 -0500

    avance

--- examenes_clinicos/admin.py
+++ examenes_clinicos/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import ProcedimientosCatalogo, ProcedimientosRealizados
 
+# Register your models here.
 
 @admin.register(ProcedimientosCatalogo)
 class ProcedimientosCatalogoAdmin(admin.ModelAdmin):
--- examenes_clinicos/models.py
+++ examenes_clinicos/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class ProcedimientosCatalogo(models.Model):
     id_procedimiento_catalogo = models.AutoField(primary_key=True)
     nombre_tipo = models.CharField(max_length=150)
-
+    descripcion = models.TextField(blank=True, null=True)
+    
     class Meta:
         managed = False
         db_table = 'procedimientos_catalogo'
