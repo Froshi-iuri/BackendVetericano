ee655c3538d665132c7eefaa3817e6d813cfd050
AnaVetericano <anyta822@gmail.com>
2026-09-10 16:55:16 -0500

    Upadte: endpoints adop, vol

--- adopciones/apiadopciones/urls.py
+++ adopciones/apiadopciones/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AdopcionAnimalViewSet
+from .views import AdopcionAnimalViewSet, SolicitudAdopcionViewSet
 
 router = DefaultRouter()
 router.register(r'adopciones', AdopcionAnimalViewSet, basename='adopcion')
+router.register(r'solicitudes-adopcion', SolicitudAdopcionViewSet, basename='solicitud-adopcion')
 
 urlpatterns = [
     path('', include(router.urls)),
--- adopciones/models.py
+++ adopciones/models.py
@@ -10,3 +10,18 @@ class AdopcionAnimal(models.Model):
 
     def __str__(self):
         return f"{self.nombre} - {self.raza}"
+    
+class SolicitudAdopcion(models.Model):
+    animal = models.ForeignKey(AdopcionAnimal, on_delete=models.CASCADE, related_name='solicitudes')
+    nombre_adoptante = models.CharField(max_length=150)
+    cedula = models.CharField(max_length=20)
+    correo_adoptante = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    mensaje = models.TextField(blank=True, null=True)
+    fecha_solicitud = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Solicitud de {self.nombre_adoptante} para {self.animal.nombre}"
+
+
+    
