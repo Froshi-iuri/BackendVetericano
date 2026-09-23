836cd506028ebd6bc5e18bd6f6d0e5c1d19ae00d
Froshi <kg5892868@gmail.com>
2026-09-23 16:28:46 -0500

    feat: new date added at response of login, in voluntariado delete column id_animal

--- users/api/views.py
+++ users/api/views.py
@@ -54,6 +54,7 @@ class LoginView(generics.GenericAPIView):
         return Response({
             "mensaje": f"Bienvenido, {user.nombre} {user.apellido}",
             "email": user.email,
+            "user": user.id_usuario,
             "id_rol": user.id_rol.id_rol,
             "tokens": {
                 "refresh": str(refresh),
